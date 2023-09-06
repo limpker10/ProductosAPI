71e3e5ab475e45f33627ef4358c86f599a34971c
Jose Cano <jcanov@unsa.edu.pe>
2023-09-05 21:38:59 -0500

    Configuración de microservicio para agregar ConfigClient

--- src/main/java/pe/edu/tecsup/productosapi/ProductosApiApplication.java
+++ src/main/java/pe/edu/tecsup/productosapi/ProductosApiApplication.java
@@ -2,8 +2,10 @@ package pe.edu.tecsup.productosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductosApiApplication {
 
 	public static void main(String[] args) {
